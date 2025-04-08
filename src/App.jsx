2d2f200e9f42643d6ade34c4fa507e94c88f615d
Sanauar <admin@bharatteleclinic.co>
2025-04-08 16:10:48 +0530

    final comment

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Experience from './components/Experience/Experience';
 import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter,Routes,Route} from "react-router-dom"
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
 function App() {
